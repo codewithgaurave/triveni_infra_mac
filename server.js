6e401859da5bc5a824072c549efe222887f28128
digyGaurav <gaurav.gupta@digy4.com>
2025-11-20 22:58:13 +0530

    done

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
-const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, "build")));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 const port = process.env.PORT || 8080;
