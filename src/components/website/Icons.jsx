c36e221acfab3227204ea674c5d0371f37b664aa
digyGaurav <gaurav.gupta@digy4.com>
2025-11-24 18:20:42 +0530

    done the changes in the

--- src/components/website/Icons.jsx
+++ src/components/website/Icons.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaWhatsapp } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa6";
 import { Phone } from 'lucide-react';
 
 const Icons = ({ isMobileMenuOpen }) => {
